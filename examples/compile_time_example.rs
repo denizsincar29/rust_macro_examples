e5559535fe8b89b6d20db753ed1dbf936a190b1a
denizsincar29 <deniz@r1oaz.ru>
2025-10-25 19:24:51 +0300

    Compile time example modified to compile at work time.

--- examples/compile_time_example.rs
+++ examples/compile_time_example.rs
@@ -2,7 +2,7 @@ use procmacros::compile_time;
 
 // This example can only be compiled during specific hours
 // Comment out or adjust the times to test
-compile_time!("00:00", "23:59", "Compilation allowed at any time for demo purposes!");
+compile_time!("18:00", "09:00", "Compilation allowed only between 9 AM and 6 PM");
 
 fn main() {
     println!("Successfully compiled! The time restriction allowed this build.");
